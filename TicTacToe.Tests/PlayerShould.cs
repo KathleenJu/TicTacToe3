6a7327ba9ef6a4ecb7ecb727a697fb915c9b54cc
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-08-06 10:10:24 +1200

    player class

---
+++ TicTacToe.Tests/PlayerShould.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TicTacToe.Tests
+{
+    public class PlayerShould
+    {
+        [Fact]
+        public void ReturnThePlayerMark()
+        {
+            
+        }
+    }
+}
